b7569edc0eafd9b2a6dac98dd88540e54d9f2f39
jiachyimlim <jiachyi.lim@gmail.com>
2020-12-27 18:32:34 +0800

    7865

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -1,11 +1,10 @@
 function GetBookings(){
-    let url = 'https://api.sheety.co/28fc25c7e1a149c6134da35538b12cd8/bookingApp/booking';
+    let url = 'https://api.sheety.co/28fc25c7e1a149c6134da35538b12cd8/bookingApp/bookings';
     fetch(url)
     .then((response) => response.json())
     .then(json => {
   // Do something with the data
   console.log(json.bookings);
-
 });
 }
 
